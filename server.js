25399d4afbe89de9c607728e239ce87e7fa9a7d8
FlorinIT <florinbest90@yahoo.com>
2017-03-12 20:18:27 +0000

    attempt to create HTML and to make it work with the server.js

--- server.js
+++ server.js
@@ -13,3 +13,21 @@ router.use(express.static(path.resolve(__dirname, 'client')));
 router.use(bodyParser.urlencoded({extended: true}));
 router.use(bodyParser.json());
 
+// GET request to dislay index.html located inside /client folder
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+// GET request to send back JSON file
+router.get('/get/json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  var obj = JSON.parse(fs.readFileSync('playerActivity.json', 'utf8'));
+  console.log("test");
+  res.end(JSON.stringify(obj));
+});
+
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
